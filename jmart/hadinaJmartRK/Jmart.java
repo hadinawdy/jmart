4bd4a8977d3f0e807aa2e2e45fedb2de675774f2
hadinawdy <90477783+hadinawdy@users.noreply.github.com>
2021-09-25 08:19:15 +0700

    revisi cs 2

---
+++ jmart/hadinaJmartRK/Jmart.java
@@ -0,0 +1,119 @@
+package hadinaJmartRK;
+
+/**
+ * Write a description of class Jmart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Jmart
+{
+    public static void main(String[] args){
+        create();
+        //Promo
+        System.out.println(getPromo());
+        //Customer
+        System.out.println(getCustomer());
+            
+        //Discount Percentage
+        System.out.println(getDiscountPercentage(1000,900));
+        System.out.println(getDiscountPercentage(1000, 0));
+        System.out.println(getDiscountPercentage(0, 0));
+        System.out.println(getDiscountPercentage(0, 1));
+            
+        //Discounted Price
+        System.out.println(getDiscountedPrice(1000, 10.0f));
+        System.out.println(getDiscountedPrice(1000, 100.0f));
+        System.out.println(getDiscountedPrice(1000, 120.0f));
+        System.out.println(getDiscountedPrice(0, 0.0f));
+            
+        //Original Price
+        System.out.println(getOriginalPrice(900, 10.0f));
+        System.out.println(getOriginalPrice(1000, 0.0f));
+        System.out.println(getOriginalPrice(0, 100.0f));
+        System.out.println(getOriginalPrice(0, 120.0f));
+            
+        //Commission Multiplier
+        System.out.println(getCommissionMultiplier());
+            
+        //Adjusted Price
+        System.out.println(getAdjustedPrice(1000));
+        System.out.println(getAdjustedPrice(500));
+        System.out.println(getAdjustedPrice(0));
+            
+        //Admin Fee
+        System.out.println(getAdminFee(1000));
+        System.out.println(getAdminFee(500));
+        System.out.println(getAdminFee(0));  
+    }
+        
+    
+    public static Product create(){
+        return new Product ("pen", 4, false, new PriceTag(10000L), ProductCategory.STATIONERY);
+    }
+    public static int getPromo(){
+        return 0;
+    }
+        
+    public static String getCustomer(){
+        return "oop";
+    }
+        
+    public static float getDiscountPercentage(int before, int after){
+        float before_ = before;
+        float after_ = after;  
+            
+            if(before_ < after_){
+                return 0.0f;
+            }
+            else{
+                float percentage = ((before_ - after_) / before_) * 100;
+                return percentage;
+            }
+    } 
+        
+    public static int getDiscountedPrice(int price, float discountPercentage){
+        float x;
+            if(discountPercentage > 100.0f){
+                return 0;
+            }
+            else {
+                return (int) (x = price - (price * (discountPercentage/100)));
+            }
+
+    }
+        
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
+        return (int) (discountedPrice * 100.0f / (100.0f - discountPercentage));
+    }
+        
+    public static float getCommissionMultiplier(){
+        return 0.05f;
+    }
+        
+    public static int getAdjustedPrice(int price){
+        float finalPrice = price + getCommissionMultiplier() * price;
+        return (int) finalPrice;
+    }
+        
+    public static int getAdminFee(int price){
+        return (int) ((float) price * getCommissionMultiplier());
+    }
+    
+    public static Product createProduct()
+    {
+        return null;
+    }
+    
+    public static Coupon createCoupun()
+    {
+        return null;
+    }
+
+    public static ShipmentDuration createShipmentDuration()
+    {
+        return null;
+    }
+}
---
+++ jmart/hadinaJmartRK/Product.java
@@ -0,0 +1,35 @@
+package hadinaJmartRK;
+
+/**
+ * Write a description of class Product here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Product
+{
+    private static int idCounter = 0;
+    public final int id;
+    public String name;
+    public int weight;
+    public boolean conditionUsed;
+    public PriceTag priceTag;
+    public ProductCategory category;
+    public ProductRating rating;
+    
+    public Product(String name, int weight, boolean conditionUsed, PriceTag priceTag, ProductCategory category)
+    {
+        this.id = idCounter++;
+        this.name = name;
+        this.weight = weight;
+        this.conditionUsed = conditionUsed;
+        this.priceTag = priceTag;
+        this.category = category;
+        this.rating = new ProductRating();
+    }
+
+    public static Product createProduct()
+    {
+        return null;
+    }
+}
